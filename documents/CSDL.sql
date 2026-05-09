76687a5e7aa6ef85cc3ca8ef4b12f423c8cc8b38
25020392-NguyenTheThanh <25020392@vnu.edu.vn>
2026-05-09 19:41:46 +0700

    CSDL

--- documents/CSDL.sql
+++ documents/CSDL.sql
@@ -1,5 +1,6 @@
 -- Xoá CSDL cũ
 DROP DATABASE IF EXISTS auction_system;
+
 -- Tạo Database auction 
 CREATE DATABASE auction_system
 	CHARACTER SET utf8mb4 -- Chỉ định bảng mã cho database (ở đây là Unicode đầy đủ)
@@ -36,3 +37,89 @@ CREATE TABLE sellers (
     ON UPDATE CASCADE -- Néu user.id thay đổi thì sellers.user_id thay đổi theo
 );
 
+-- Bảng Items (sản phẩm đấu giá)
+CREATE TABLE items (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    owner_id INT NOT NULL, -- id seller tạo ra item
+    name VARCHAR(200) NOT NULL,
+    description TEXT, -- Mô tả sản phẩm
+    starting_price DECIMAL(18,2) NOT NULL,
+    category ENUM('ART', 'ELECTRONICS', 'VEHICLE') NOT NULL,
+    status ENUM('AVAILABLE','IN_AUCTION','SOLD') NOT NULL DEFAULT 'AVAILABLE',
+-- Thuộc tính riêng của từng loại
+	artist VARCHAR(100),  -- chỉ art
+    brand VARCHAR(100),  -- chỉ electronics
+	manufacture_year SMALLINT, -- chỉ vehicle 
+	created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, -- tự lưu thời gian tạo item
+    CONSTRAINT fk_item_seller FOREIGN KEY (owner_id) REFERENCES users(id) 
+	ON DELETE RESTRICT -- Không cho xoá user nếu còn item
+    ON UPDATE CASCADE -- Néu user.id thay đổi thì item.owner_id thay đổi theo
+);
+
+-- Bảng Auction (phiên đấu giá)
+CREATE TABLE auctions (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    item_id INT NOT NULL UNIQUE, -- item được đem đấu giá
+    current_highest_bid DECIMAL(18,2) NOT NULL, -- Giá đấu cao nhất hiện tại
+    current_winner_id INT DEFAULT NULL, -- Mới tạo là null
+    status ENUM('OPEN', 'RUNNING', 'FINISHED', 'PAID', 'CANCELED') NOT NULL DEFAULT 'OPEN',
+	end_time DATETIME NOT NULL, -- Thời gian kết thúc đấu giá
+    min_bid_step DECIMAL(18,2) NOT NULL DEFAULT 0.00, -- Giá đấu đặt tối thiểu
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, 
+    CONSTRAINT fk_auction_item FOREIGN KEY (item_id) REFERENCES items(id)
+	ON DELETE RESTRICT 
+	ON UPDATE CASCADE,
+    CONSTRAINT fk_auction_winner  FOREIGN KEY (current_winner_id) REFERENCES users(id) -- Người thắng hiện tại phải tồn tại trong users
+	ON DELETE SET NULL -- Nếu user thắng bị xoá thì current_winner_id = NULL
+    ON UPDATE CASCADE 
+);
+
+-- Bảng bid_transactions (lịch sử đặt giá — BidTransaction)
+CREATE TABLE bid_transactions (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    auction_id INT NOT NULL,
+    bidder_id INT NOT NULL,
+    bidder_name VARCHAR(50) NOT NULL,
+    amount DECIMAL(18,2) NOT NULL,
+    bid_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_bid_auction FOREIGN KEY (auction_id) REFERENCES auctions(id)
+	ON DELETE CASCADE 
+    ON UPDATE CASCADE,
+    CONSTRAINT fk_bid_bidder  FOREIGN KEY (bidder_id)  REFERENCES users(id)
+	ON DELETE RESTRICT 
+    ON UPDATE CASCADE,
+    INDEX idx_auction_time (auction_id, bid_time)
+);
+
+-- Bảng bidder_watchlist (danh sách phiên theo dõi)
+CREATE TABLE bid_watchlist (
+	bidder_id INT NOT NULL,
+    auction_id INT NOT NULL,
+    PRIMARY KEY (bidder_id, auction_id),
+    CONSTRAINT fk_wl_bidder  FOREIGN KEY (bidder_id)  REFERENCES users(id)   
+    ON DELETE CASCADE,
+    CONSTRAINT fk_wl_auction FOREIGN KEY (auction_id) REFERENCES auctions(id) 
+    ON DELETE CASCADE
+);
+
+-- Bảng bidder_participated  (phiên đã từng đặt giá)
+CREATE TABLE bidder_participated (
+    bidder_id INT NOT NULL,
+    auction_id INT NOT NULL,
+    PRIMARY KEY (bidder_id, auction_id),
+    CONSTRAINT fk_part_bidder  FOREIGN KEY (bidder_id)  REFERENCES users(id)   
+    ON DELETE CASCADE,
+    CONSTRAINT fk_part_auction FOREIGN KEY (auction_id) REFERENCES auctions(id) 
+    ON DELETE CASCADE
+);
+
+-- Bảng bidder_won (phiên đã thắng)
+CREATE TABLE bidder_won (
+	bidder_id  INT NOT NULL,
+    auction_id INT NOT NULL,
+    PRIMARY KEY (bidder_id, auction_id),
+    CONSTRAINT fk_won_bidder FOREIGN KEY (bidder_id) REFERENCES users(id)   
+    ON DELETE CASCADE,
+    CONSTRAINT fk_won_auction FOREIGN KEY (auction_id) REFERENCES auctions(id) 
+    ON DELETE CASCADE
+);
