97892ccca4cbe592af776f3e4b9e0db33d148b8b
son <luuvanhoangson@gmail.com>
2026-05-26 22:34:35 +0700

    nãy a đổi nhầm (thanh)

--- src/main/java/com/auction/client/ServerConnection.java
+++ src/main/java/com/auction/client/ServerConnection.java
@@ -15,7 +15,7 @@ import java.util.function.Consumer;
 public class ServerConnection {
 
     // Địa chỉ server
-    public static final String DEFAULT_HOST = "sql12.freesqldatabase.com";
+    public static final String DEFAULT_HOST = "localhost";
 
     // Port server
     public static final int DEFAULT_PORT = 9999;
