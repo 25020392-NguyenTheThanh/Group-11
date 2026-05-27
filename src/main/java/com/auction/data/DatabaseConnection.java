70f65ce14955f9186d92334329f031532b2d16d6
Tien <duongdz12345789@gmail.com>
2026-05-27 14:04:04 +0700

    sửa database

--- src/main/java/com/auction/data/DatabaseConnection.java
+++ src/main/java/com/auction/data/DatabaseConnection.java
@@ -11,10 +11,15 @@ import java.sql.SQLException;
 public class DatabaseConnection {
 
     private static final String DB_URL = System.getenv().getOrDefault(
-            "DB_URL",
-            "jdbc:mysql://localhost:3306/auction_system?useUnicode=true&characterEncoding=utf8&serverTimezone=Asia/Ho_Chi_Minh");
-    private static final String DB_USER = System.getenv().getOrDefault("DB_USER", "root");
-    private static final String DB_PASS = System.getenv().getOrDefault("DB_PASS", "ngtien123@@@");
+            "DB_URL", "jdbc:mysql://sql12.freesqldatabase.com:3306/sql12828332"
+                    + "?useUnicode=true"
+                    + "&characterEncoding=utf8"
+                    + "&serverTimezone=Asia/Ho_Chi_Minh"
+                    + "&useSSL=false"
+                    + "&allowPublicKeyRetrieval=true"
+    );
+    private static final String DB_USER = System.getenv().getOrDefault("DB_USER", "sql12828332");
+    private static final String DB_PASS = System.getenv().getOrDefault("DB_PASS", "lBL6xIvN17");
 
     private static DatabaseConnection instance;
 
